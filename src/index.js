d31f2d833ef1d973851034b04ba5a4014ee8a46f
nittosantos <nitto_kun@hotmail.com>
2022-07-12 09:19:28 -0300

    adicionando os todos aos usuários

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post("/users", (request, response) => {
@@ -36,11 +46,30 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+  const { user } = request;
+
+  const isUser = users.find((user) => user.username === username);
+
+  const todosOperations = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  if (isUser) {
+    user.todos.push(todosOperations);
+  }
+
+  return response.status(201).send();
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
